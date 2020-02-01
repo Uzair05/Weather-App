3a16be5319bd78f4f674f728c8fedc55784cfc66
Uzair Bin Colangoy Asim <u3560307@connect.hku.hk>
2020-02-01 16:11:29 +0500

    1

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,29 +1,29 @@
-var express  = require('express');
+var express = require('express');
 var $ = require('jquery');
 var app = express();
 
 
-app.use(express.static('public'), function(req,res,next){
-    next();
+app.use(express.static('public'), function(req, res, next) {
+  next();
 })
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile('/index.html');
-  //res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.post('/Weather',function(req,res){
+
+
+app.post('/Weather', function(req, res) {
   /*Get Data and return json array*/
   var lat = req.lat;
   var lon = req.lon;
 
-  console.log(lat+"; "+lon);
+  console.log(lat + "; " + lon);
 
 });
 
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(8081, function() {
   var host = server.address().address
   var port = server.address().port
   console.log("Example app listening at http://%s:%s", host, port)
--- WeatherApp/public/javascripts/index.js
+++ WeatherApp/public/javascripts/index.js
@@ -1,13 +1,23 @@
 var x = document.getElementById("demo");
-var latitude;
-var longitude;
+
 
 
 
 function learnt(){
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position){
-      x.innerHTML = "latitude =" + position.coords.latitude +"; " + "longitude = "  + position.coords.longitude;
+       var latitude = position.coords.latitude;
+       var longitude = position.coords.longitude;
+
+
+             /*
+
+             Enter Codes To get API reponse here.
+
+              */
+
+       x.innerHTML = "latitude =" + latitude +"; " + "longitude = "  + longitude;
+
     });
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
