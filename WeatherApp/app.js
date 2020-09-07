3e8e320706418095894cedb4a03fcf0b79f9dadb
Uzair Bin Colangoy Asim <u3560307@connect.hku.hk>
2020-09-07 20:22:30 +0500

    refreshed API keys

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -14,7 +14,7 @@ app.post('/Weather', express.urlencoded({extended: true}), function(req, res) {
   var latitude = req.body.latitude;
   var longitude = req.body.longitude;
   const https = require('https');
-  https.get('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=33f2983a721f872ef3cbaf1619646581', (resp) => {
+  https.get('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=c5c81d1d2b51807c753d34b4ab9227db', (resp) => {
     let data = '';
     // A chunk of data has been recieved.
     resp.on('data', (chunk) => {
@@ -43,7 +43,7 @@ app.post('/WeatherForecast', express.urlencoded({extended: true}), function(req,
   const https = require('https');
 
 
-  https.get('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=33f2983a721f872ef3cbaf1619646581', (resp) => {
+  https.get('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=c5c81d1d2b51807c753d34b4ab9227db', (resp) => {
     let data = '';
     // A chunk of data has been recieved.
     resp.on('data', (chunk) => {
